7c462b3c3f75298c2388c3319adae95dacde212d
weoil <weoil@live.cn>
2019-09-07 13:43:18 +0800

    合并代码，rule配置中增加最大连接数限制

--- src/middleware/repeat.ts
+++ src/middleware/repeat.ts
@@ -4,38 +4,9 @@ interface cacheMapImp {
 export default async function noRepeat(
   config: Http.MiddlewareConfig
 ): Promise<Http.MiddlewareConfig | false> {
-<<<<<<< HEAD
-	if (config.repeat) {
-		return config;
-	}
-	let { url, overlist, cacheMap, rule, rootConfig, cacheTime } = config;
-	if (!overlist) {
-		rootConfig.overlist = config.overlist = overlist = new Set();
-	}
-	if (!cacheMap) {
-		rootConfig.cacheMap = config.cacheMap = cacheMap = new Map<
-			string,
-			cacheMapImp
-		>();
-	}
-	const $cacheTime = cacheTime || (rule && rule.config.cacheTime);
-	if ($cacheTime) {
-		const $cache = cacheMap.get(url);
-		if ($cache && Date.now() - $cache.date >= $cacheTime) {
-			$cache.date = Date.now();
-			return config;
-		} else if (!$cache && !overlist.has(url)) {
-			overlist.add(url);
-			cacheMap.set(url, { date: Date.now() });
-			return config;
-		}
-		return false;
-	}
-	if (overlist.has(url)) {
-		return false;
-	}
-	return config;
-=======
+  if (config.repeat) {
+    return config;
+  }
   let { url, overlist, cacheMap, rule, rootConfig, cacheTime } = config;
   if (!overlist) {
     rootConfig.overlist = config.overlist = overlist = new Set();
@@ -46,7 +17,7 @@ export default async function noRepeat(
       cacheMapImp
     >();
   }
-  const $cacheTime = cacheTime || (rule && rule.cacheTime);
+  const $cacheTime = cacheTime || (rule && rule.config.cacheTime);
   if ($cacheTime) {
     const $cache = cacheMap.get(url);
     if ($cache && Date.now() - $cache.date >= $cacheTime) {
@@ -63,5 +34,4 @@ export default async function noRepeat(
     return false;
   }
   return config;
->>>>>>> 459e0e5e3b09a16ae8cc8e516cfe7bc0f866f73f
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,19 +2,6 @@ import request from 'request';
 export interface CoreOptions extends request.CoreOptions {}
 
 export interface HttpConfig extends CoreOptions {
-<<<<<<< HEAD
-	name?: string;
-	url?: string;
-	retry?: number;
-	meta?: {
-		[key: string]: any;
-	};
-	charset?: string;
-	rule: Rule.Rule;
-	cacheTime?: number;
-	overlist?: Set<string>;
-	[key: string]: any;
-=======
   name?: string;
   url?: string;
   retry?: number;
@@ -22,9 +9,8 @@ export interface HttpConfig extends CoreOptions {
     [key: string]: any;
   };
   charset?: string;
-  rule?: Rule.Config;
+  rule: Rule.Rule;
   cacheTime?: number;
   overlist?: Set<string>;
   [key: string]: any;
->>>>>>> 459e0e5e3b09a16ae8cc8e516cfe7bc0f866f73f
 }
